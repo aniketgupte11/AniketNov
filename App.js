cfc03573e79cbd7ae29c3dbdd16acab0c34a2751
Aniket Gupte <aniketgupte11@gmail.com>
2025-11-07 18:21:33 +0530

    Day2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const headings = React.createElement("h1", { id: "heading" }, "h1 from React");
 // in the 3 elements 1 is the type of element, 2nd is the attributes and 3rd is the content inside the element
 const parentTag = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child", key: "parent" }, [
     React.createElement("h1", { key: "h1" }, "I am an nested H1 Tag"),
     React.createElement("h2", { key: "h2" }, "I am an nested H2 Tag"),
   ]),
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child", key: "child" }, [
     React.createElement("h1", { key: "h1" }, "I am an nested H1 Tag"),
     React.createElement("h2", { key: "h2" }, "I am an nested H2 Tag"),
   ]),
--- README.md
+++ README.md
@@ -27,3 +27,21 @@ Merge main into your branch (safer)
 9.  git checkout your-branch
     git fetch origin
     git merge origin/main
+
+    \*\*\*------------------------------------------------------------------------------
+
+    To start adding a server
+
+    1. npm init
+    2. start adding the requirements that it asks and add jest for testing
+    3. Confirm the github repo
+    4. keywords:- may be React, Nov React or anything
+    5. Author:- Write your name
+
+    This adds a package.json
+
+    \*\*\*-----------------------------------------------------------------------------
+
+    Next add a bundler:- ie parcel. We can do create react app or vite also. But here its parcel that is being used.
+    Once parcel is installed. Next install React and react-dom.
+    To run parcel:- npx parcel index.html
