26aa2692d699618e610273512d46106abcea8246
Aniket Gupte <aniketgupte11@gmail.com>
2025-11-13 11:05:04 +0530

    App js updated with element and component basics

--- App.js
+++ App.js
@@ -22,9 +22,36 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", { id: "h1" }, "React is awesome!");
 // If we have to add multiple lines of code in JSX then we have to wrap it inside parenthesis
+
+// React Element using JSX
 const headingJSX = <h1 className="headingJSX">React is awesome using JSX</h1>;
+
+// React Functional Component using JSX
+// Component Composition - When a component is created inside another component
+const Title = function () {
+  return (
+    <div>
+      <h1>This is title component 🤖</h1>
+    </div>
+  );
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      {headingJSX}
+      <h1> This is a Functional Component</h1>
+    </div>
+  );
+};
+
 console.log(heading);
 console.log(headingJSX);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(heading);
-root.render(headingJSX);
+//root.render(headingJSX);
+root.render(<HeadingComponent />);
+
+//Note:- If we have any react element then it is rendered as the name.
+//But if the react element is a component then it is rendered as <ComponentName />
